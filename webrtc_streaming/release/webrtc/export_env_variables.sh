1ea612c08448ad7a923ff828647310a1a147b6e0
swapnilwarkar <swapnilwarkar@gmail.com>
2022-04-04 14:16:14 +0530

    created release directory for webrtc streaming

---
+++ webrtc_streaming/release/webrtc/export_env_variables.sh
@@ -0,0 +1,8 @@
+export PKG_CONFIG_PATH=/home/swapnil/Desktop/video_streaming/webrtc_streaming/gst-dev-20/lib/x86_64-linux-gnu/pkgconfig/
+export GST_PLUGIN_PATH=/home/swapnil/Desktop/video_streaming/webrtc_streaming/gst-dev-20/lib/x86_64-linux-gnu/gstreamer-1.0/
+export LD_LIBRARY_PATH=/home/swapnil/Desktop/video_streaming/webrtc_streaming/gst-dev-20/lib/x86_64-linux-gnu/
+export PATH=/home/swapnil/Desktop/video_streaming/webrtc_streaming/gst-dev-20/bin:$PATH
+
+
+
+
